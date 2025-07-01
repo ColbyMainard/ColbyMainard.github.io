b501571dc5156b89cd616db0469031fed2318e16
ColbyMainard <cmythman@gmail.com>
2025-07-01 12:45:47 -0500

    Improving JS and adding Google Analytics

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,33 @@
 /* https://www.w3schools.com/js/DEFAULT.asp */
 
-function modifyElementID(id, contents){
-    document.getElementById(id).innerHTML = contents;
+var datetime = new Date();
+console.log("Current Time:");
+console.log(datetime.toISOString());
+
+/* https://www.w3schools.com/js/js_class_intro.asp */
+
+class Data{
+    constructor(){}
+    /* https://www.w3schools.com/js/js_class_static.asp */
+    static editDocument(elementID, contents){
+        document.getElementById(elementID).innerHTML = contents;
+    }
 }
+
+setTimeout(
+    Data.editDocument(
+        "contactMe", 
+        document.getElementById(
+            "contactMe"
+        ).innerHTML
+    ), 
+    3000
+);
+
+/* https://www.w3schools.com/js/js_callback.asp */
+
+/* https://www.w3schools.com/js/js_asynchronous.asp */
+
+/* https://www.w3schools.com/js/js_promise.asp */
+
+/* https://www.w3schools.com/js/js_async.asp */
