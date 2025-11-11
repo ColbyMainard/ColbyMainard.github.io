998cdb0f069584d1a4f5959fa4246917c72a4644
ColbyMainard <cmythman@gmail.com>
2025-11-11 14:25:34 -0600

    Improved color scheme and graphics

--- assets/js/send_email.js
+++ assets/js/send_email.js
@@ -1,6 +1,6 @@
 function send_email(){
     const my_email = 'colby.mainard@proton.me';
     const subject = 'Introducing Myself';
-    const body = 'Hi Colby,\n\nI saw your website and wanted to reach out.\n\nBest,';
+    const body = 'Hi Colby,\r\n\r\nI saw your website and wanted to reach out.\r\n\r\nBest,';
     window.open(`mailto:${my_email}?subject=${subject}&body=${body}`);
 }
