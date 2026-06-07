cfd1798bb94d65bcad313f95581e14c9ea96b722
ColbyMainard <cmythman@gmail.com>
2026-06-06 20:34:18 -0500

    Updating content to attract backlinks

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * fallback. Supports offline browsing and speeds up repeat visits.
  */
 
-const CACHE_VERSION = "v15";
+const CACHE_VERSION = "v16";
 const CACHE_NAME = "colbymainard-" + CACHE_VERSION;
 
 const PRECACHE_URLS = [
