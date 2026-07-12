67dd1a1500f1257c966be59b452f01d24d165ed3
ColbyMainard <cmythman@gmail.com>
2026-07-12 16:50:42 -0500

    Link optimizations

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * fallback. Supports offline browsing and speeds up repeat visits.
  */
 
-const CACHE_VERSION = "v36";
+const CACHE_VERSION = "v37";
 const CACHE_NAME = "colbymainard-" + CACHE_VERSION;
 
 const PRECACHE_URLS = [
