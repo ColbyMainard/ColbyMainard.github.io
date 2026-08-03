df8773a851f5641ca22bf73fb49d254c5de9fe90
ColbyMainard <cmythman@gmail.com>
2026-08-02 21:30:31 -0500

    Minor additions

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * fallback. Supports offline browsing and speeds up repeat visits.
  */
 
-const CACHE_VERSION = "v52";
+const CACHE_VERSION = "v53";
 const CACHE_NAME = "colbymainard-" + CACHE_VERSION;
 
 const PRECACHE_URLS = [
